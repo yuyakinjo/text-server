bb60fb9b682db274c0c90b6909d71da831770372
yuya <kinjo.bobchin@gmail.com>
2023-12-01 13:29:47 +0900

    fix: Fix incorrect regular expression in jsonlength.ts
    The regular expression used to count the number of JSON objects was incorrect. It was counting the number of opening brackets instead of closing brackets. This has been fixed by updating the regular expression to match closing brackets.

--- src/jsonlength.ts
+++ src/jsonlength.ts
@@ -2,31 +2,32 @@ import { Worker } from 'worker_threads';
 import { Message } from './worker';
 import { statSync } from 'fs';
 import { cliOptions } from './cli-options';
-
+import { size } from './kbsize';
 const { filename, workerNum } = cliOptions;
-
 console.time('jsonlength');
-
 const fileSize = statSync(filename).size;
+const byte = size(filename);
+
 if (!fileSize) {
   throw new Error('File is not fount or empty.');
 }
-let assignWokerNum = Number(workerNum); // 並列に実行するWorkerの数を指定します。この数値はCPUのコア数に応じて調整してください。
-let chunkSize = Math.ceil(fileSize / assignWokerNum);
-
+let assignWokerNum = Number(workerNum);
+let chunkSize = Math.floor(fileSize / assignWokerNum);
 let rows = 0;
 let finishedWorkerCounter = 0;
-
+let finishedChunkSize = 0;
 for (let i = 0; i < assignWokerNum; i++) {
   const start = i * chunkSize;
-  const end = i === assignWokerNum - 1 ? fileSize : start + chunkSize;
+  const end = i === assignWokerNum - 1 ? fileSize : (i + 1) * chunkSize;
   const worker = new Worker('./src/worker.ts', { workerData: { filePath: filename, start, end } });
   worker.on('message', ({ count }: Message) => {
     rows += count;
     finishedWorkerCounter++;
+    finishedChunkSize += end - start;
     if (finishedWorkerCounter === assignWokerNum) {
       console.timeEnd('jsonlength');
     }
     console.log(`Worker ${i} finished. Count is ${count}. Total count is ${rows}.`);
+    console.log({ i, finishedChunkSize, fileSize });
   });
 }
--- src/worker.ts
+++ src/worker.ts
@@ -6,15 +6,16 @@ import { parentPort, workerData } from 'worker_threads';
 export type Message = Record<'count', number>;
 
 const { filePath, start, end } = workerData;
-const fd = openSync(filePath, 'r');
+const readonly = 'r';
+const fd = openSync(filePath, readonly);
 const buffer = Buffer.allocUnsafe(end - start);
 readSync(fd, buffer, 0, end - start, start);
 const textLikeJson = buffer.toString();
 
 const connectionPartOfJson = new RegExp(/},/gm);
-const firstJson = new RegExp(/\[{/);
+const lastJson = new RegExp(/\}]/);
 
-const startStrings = (textLikeJson.match(firstJson) || []).length;
+const startStrings = (textLikeJson.match(lastJson) || []).length;
 const connections = (textLikeJson.match(connectionPartOfJson) || []).length;
 
 const count = connections + startStrings;
